94dd4cd8ef643ebfc85946c77c435c21a1da789b
Alice A <theblackhairday@hotmail.com>
2022-05-31 21:03:52 +0200

    drawer

--- lib/widgets/styled_drawer.dart
+++ lib/widgets/styled_drawer.dart
@@ -8,72 +8,64 @@ class StyledDrawer extends StatefulWidget {
 }
 
 class _DrawerState extends State<StyledDrawer> {
+  List menu = List.generate(
+    4,
+    (int index) {
+      return Container(
+        margin: const EdgeInsets.only(top: 24),
+        decoration: BoxDecoration(
+            color: Colors.greenAccent.shade200,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(0),
+            ),
+            border: Border.all(color: Colors.black, width: 3),
+            boxShadow: const [
+              BoxShadow(
+                spreadRadius: 0,
+                blurRadius: 0,
+                offset: Offset(3, 6),
+              )
+            ]),
+        child: ListTile(
+          style: ListTileStyle.list,
+          title: Center(
+              child: Text(
+            'Item ${index + 1}',
+            style: const TextStyle(fontSize: 16, color: Colors.black),
+          )),
+        ),
+      );
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: const Color(0xfff5f2d9),
-      child: Column(
-        verticalDirection: VerticalDirection.down,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: DrawerHeader(
-                margin: EdgeInsets.zero,
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple.shade300,
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(0),
-                      bottomRight: Radius.circular(100)),
-                  border: Border.all(color: Colors.black, width: 3),
-                ),
-                child: const Text(
-                  'Hello!',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+      child: ListView(
+          padding: const EdgeInsets.only(right: 24, left: 24),
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: DrawerHeader(
+                  margin: EdgeInsets.zero,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple.shade300,
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(0),
+                        bottomRight: Radius.circular(100)),
+                    border: Border.all(color: Colors.black, width: 3),
                   ),
-                )),
-          ),
-          Expanded(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: ListView(
-                  padding: const EdgeInsets.only(right: 24, left: 24),
-                  children: List.generate(
-                    4,
-                    (int index) {
-                      return Container(
-                        margin: const EdgeInsets.only(top: 24),
-                        decoration: BoxDecoration(
-                            color: Colors.greenAccent.shade200,
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(0),
-                            ),
-                            border: Border.all(color: Colors.black, width: 3),
-                            boxShadow: const [
-                              BoxShadow(
-                                spreadRadius: 0,
-                                blurRadius: 0,
-                                offset: Offset(3, 6),
-                              )
-                            ]),
-                        child: ListTile(
-                          style: ListTileStyle.list,
-                          title: Center(
-                              child: Text(
-                            'Item ${index + 1}',
-                            style: const TextStyle(
-                                fontSize: 16, color: Colors.black),
-                          )),
-                        ),
-                      );
-                    },
+                  child: const Text(
+                    'Hello!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   )),
             ),
-          )
-        ],
-      ),
+            ...menu,
+          ]),
     );
   }
 }
