d9703dd64208a2959edd182b09ec57efe9968b7e
Alice A <theblackhairday@hotmail.com>
2022-06-02 19:37:52 +0200

    sanity and drawer

--- lib/widgets/styled_drawer.dart
+++ lib/widgets/styled_drawer.dart
@@ -51,7 +51,7 @@ class _DrawerState extends State<StyledDrawer> {
                 color: Colors.deepPurple.shade300,
                 borderRadius: const BorderRadius.only(
                     topRight: Radius.circular(0),
-                    bottomRight: Radius.circular(100)),
+                    bottomRight: Radius.circular(80)),
                 border: Border.all(color: Colors.black, width: 3),
                 boxShadow: const [
                   BoxShadow(
@@ -61,12 +61,35 @@ class _DrawerState extends State<StyledDrawer> {
                   )
                 ],
               ),
-              child: const Text(
-                'Hello!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hello!',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      margin: const EdgeInsets.only(top: 8),
+                      decoration: BoxDecoration(
+                        color: Colors.amber.shade300,
+                        border: Border.all(color: Colors.black, width: 4),
+                      ),
+                      child: const Text(
+                        'Alice Anglesjö',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ]),
+                ],
               )),
         ),
         ...menu,
